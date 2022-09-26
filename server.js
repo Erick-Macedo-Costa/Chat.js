a1e3d11068e6250988368b09140c43beb6eee38d
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-09-26 19:36:01 -0300

    Commit inicial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+const socket = require ("socket.io")
+const http = require("http").Server(app)
+const  PORT = 8000
+const serverSocket = require("socket.io")(http)
+
+serverSocket.on("connect", socket => {
+    console.log(`Cliente ${socket.id} conectou`)
+    socket.on('chat msg', msg => {
+        serverSocket.emit("chat msg", `Mensagem recebida de ${socket.username}: ${msg}`)
+    })
+
+    socket.on("login", username => {
+        socket.username = username
+        serverSocket.emit("chat msg", `Usuario ${username} entrou`)
+    })
+})
+
+http.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
+app.get("/", (_,res)=> res.sendFile(`${__dirname}/index.html`))
